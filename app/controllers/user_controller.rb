90ba92e272e6cc4758ae7183b2f200430aeb4ba1
wchow1983 <Wchow@chicagobooth.edu>
2021-11-11 06:14:53 +0000

    All Pages Except Add and Delete Completed

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -14,7 +14,12 @@ class UserController < ApplicationController
     the_username = params.fetch("username")
     @the_user = User.where({:username => the_username}).first
 
-    all_photos = Photo.all
+    #all_photos = Photo.all 
+
+    #"all_photos = Photo.all" is not necessary because in the show.html.erb template, we are only 
+    #referencing the photo database for the "Own photos" section which utilized the User class and 
+    #all the instance methods in its respective User "/app/models". This includes "own_photos" 
+    #which accesses the Photo database (i.e. Photo.where).  
 
     render({ :template => "user_templates/show.html.erb"})
   end
